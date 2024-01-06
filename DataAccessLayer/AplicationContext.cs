3dd4473c11524571a8b6c252b6224e9a9bc2e90f
Murad5455 <118770310+Murad5455@users.noreply.github.com>
2024-01-06 19:32:07 +0400

    Test Commit

--- DataAccessLayer/AplicationContext.cs
+++ DataAccessLayer/AplicationContext.cs
@@ -5,31 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*
-namespace DataAccessLayer
-{
-    public class AplicationContext : DbContext
-    {
-        readonly string _connectionString;
-
-
-        *//*public AplicationContext(string connectionString)
-        {
-            _connectionString = connectionString;
-        }*//*
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("server=DESKTOP-MSI1S2I;database=Agile_Task;Integrated Security=true");
-        }
-
-        public DbSet<InfoData> InfoDatas { get; set; }
-
-    }
-}
-*/
-
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace DataAccessLayer
@@ -50,6 +25,7 @@ namespace DataAccessLayer
         }
 
         public DbSet<InfoData> InfoDatas { get; set; }
+        public DbSet<InfoData> InfoDatasa { get; set; }
     }
 }
 
